a2ea44f874633e55a85d5e2c9d85f833702414d8
Alex Hall <alex.mojaki@gmail.com>
2022-01-17 11:38:27 +0200

    Move skipWaiting and clients.claim to test/sw.js

--- test/sw.js
+++ test/sw.js
@@ -1,3 +1,11 @@
 import * as lib from "../lib"
 
 lib.setupServiceWorker();
+
+addEventListener('install', function (e) {
+  e.waitUntil(self.skipWaiting());
+});
+
+addEventListener('activate', function (e) {
+  e.waitUntil(self.clients.claim());
+});
