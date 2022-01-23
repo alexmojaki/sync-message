3b0733c0f0227dfb9c2e5e18b14b4e870063ea23
Alex Hall <alex.mojaki@gmail.com>
2022-01-23 14:34:03 +0200

    randomString instead of uuidv4

--- test/index.js
+++ test/index.js
@@ -16,9 +16,9 @@ async function init() {
 
   for (const {channel, writeInput} of channels) {
     for (let i = 0; i < 100; i++) {
-      const messageId = uuidv4();
+      const messageId = randomString();
+      const message = randomString();
       const readPromise = testWorker(channel, messageId);
-      const message = Math.random() + " " + new Date();
       await writeInput(message, messageId);
       const response = await readPromise;
       console.log(response === message);
@@ -26,11 +26,8 @@ async function init() {
   }
 }
 
-// https://stackoverflow.com/a/2117523/2482744
-export function uuidv4() {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  );
+export function randomString() {
+  return `${+new Date()} ${Math.random()} ${Math.random()} ${Math.random()}`
 }
 
 init();
